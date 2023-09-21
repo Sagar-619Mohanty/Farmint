6a558301967450f0fa43b865f6292fac8d15e75a
Sagar-619Mohanty <143720093+Sagar-619Mohanty@users.noreply.github.com>
2023-09-21 19:22:03 +0530

    Add files via upload

---
+++ Farmint_App.java
@@ -0,0 +1,29 @@
+public class Farmint_App {
+    public String Buyer_app;
+    public String Seller_app;
+    public String All_Customised_App;
+
+    public String getBuyer_app() {
+        return Buyer_app;
+    }
+
+    public void setBuyer_app(String buyer_app) {
+        Buyer_app = buyer_app;
+    }
+
+    public String getSeller_app() {
+        return Seller_app;
+    }
+
+    public void setSeller_app(String seller_app) {
+        Seller_app = seller_app;
+    }
+
+    public String getAll_Customised_App() {
+        return All_Customised_App;
+    }
+
+    public void setAll_Customised_App(String all_Customised_App) {
+        All_Customised_App = all_Customised_App;
+    }
+}
